fd72893f9035762159cb09eb6d38ae8d73f7d5cd
Gourav Goyal <gouravgoyal290@gmail.com>
2020-05-31 21:52:59 +0530

    Responsive some part left

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -10,7 +10,43 @@ const Technologies =() =>{
             <div class={styles.tech__headingBlock}>
                  <h3 class={styles.tech__headingS}>Industries</h3>
             </div>
-            <div className={styles.tech__container}> 
+            {/* for mobile */}
+            <div className={styles.tech__m__container}>
+                <div className={styles.tech__m__column}>
+                    <ul className={styles.tech__list}>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/hospitality.png" alt="tech"/></span>
+                            Real Estate
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/legal.png" alt="tech"/></span>
+                            Legal
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/travel.png" alt="tech"/></span>
+                            Travel
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.tech__m__column}>    
+                    <ul className={styles.tech__list}>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/healthcare.png" alt="tech"/></span>
+                            Healthcare
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/financial.png" alt="tech"/></span>
+                            Finance
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/automotive.png" alt="tech"/></span>
+                            Automotive
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {/* for web */}
+            <div className={styles.tech__container}>
                 <div className={styles.tech_column}>
                     <ul className={styles.tech__list}>
                         <li className={styles.tech__item}>
@@ -52,6 +88,82 @@ const Technologies =() =>{
             <div class={styles.tech__headingBlock}>
                  <h3 class={styles.tech__headingS}>Technologies</h3>
             </div>
+            {/* for mobile */}
+            <div className={styles.tech__m__container2}>
+                <div className={styles.tech__m__column}>
+                    <ul className={styles.tech__list}>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/nodejs.png" alt="tech"/></span>
+                            Node.js
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/ror.png" alt="tech"/></span>
+                            Ruby on Rails
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/python.png" alt="tech"/></span>
+                            Python
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/expressjs.png" alt="tech"/></span>
+                            Express.js
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/meteorjs.png" alt="tech"/></span>
+                            Meteor.js
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/uiux.png" alt="tech"/></span>
+                            UI/UX
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/discovery.png" alt="tech"/></span>
+                            Product Discovery
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/design.png" alt="tech"/></span>
+                            Product Design
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/prototyping.png" alt="tech"/></span>
+                            Rapid Prototyping
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.tech__m__column}>
+                <ul className={styles.tech__list}>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/reactjs.png" alt="tech"/></span>
+                            React.js
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/angularjs.png" alt="tech"/></span>
+                            Angular.js
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/reactjs.png" alt="tech"/></span>
+                            React Native
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/swift.png" alt="tech"/></span>
+                            Swift
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/cloud.png" alt="tech"/></span>
+                            Cloud
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/automation.png" alt="tech"/></span>
+                            Automation
+                        </li>
+                        <li className={styles.tech__item}>
+                            <span> <img width="22px" src="http://thirdessential.com/coax/assets/images/vector-icon/monitoring.png" alt="tech"/></span>
+                            Monitoring
+                        </li>     
+                    </ul>
+                </div>
+            </div>
+            {/* for web */}
             <div className={styles.tech__container2}> 
                 <div className={styles.tech_column}>
                     <ul className={styles.tech__list}>
